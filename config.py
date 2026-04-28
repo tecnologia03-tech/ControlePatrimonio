dfe6fc6cc7ed20d1ce17e94c154eee318ccd366d
tecnologia03-tech <tecnologia_03@cssa.com.br>
2026-04-28 07:26:06 -0400

    adicionando comentários no CONFIG

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # config.py
+# Carrega as variáveis de ambiente do arquivo .env
 from dotenv import load_dotenv
 import os
 
+# Lê a URL de conexão com o banco de dados PostgreSQL
 load_dotenv()
 DB_URL = os.getenv("DATABASE_URL")
